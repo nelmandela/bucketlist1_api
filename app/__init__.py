118f75ea2b7c7448575c8ad63d8ef36b82e7c543
nelmandela <nelsonmandela784@gmail.com>
2017-09-07 18:25:04 +0300

    [Feature] create user model - add sqlachemy - bind the db object to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
 from config import app_configuration
+from models import db
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True, static_folder=None)
     app.config.from_object(app_configuration[config_name])
+
+    db.init_app(app)
     return app
---
+++ app/models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLALCHEMY
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLALCHEMY()
+
+
+class User(db.Model):
+    """Define user properties."""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
