907b6b707d01117cbf6156d6e8a89dc2f475127e
nelmandela <nelsonmandela784@gmail.com>
2017-09-08 15:53:04 +0300

    [FEATURE] Add bucketlist and item resources to models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 db = SQLAlchemy()
 
@@ -12,6 +15,8 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password_hash = db.Column(db.String(128))
+    buckelists = db.relationship('Bucketlists', backref='users',
+                                cascade='all, delete')
 
     @property
     def password(self):
@@ -35,3 +40,55 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User: {}>'.format(self.username)
+    def save(self):
+        """Define """
+        db.session.add(self)
+        db.session.commit
+
+class Bucketlist(db.Model):
+    """Define buckelist properties."""
+    __tablename__ = "bucketlists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_on = db.Column(db.DateTime, default=datetime.now())
+    items = db.relationship('Items', backref='bucketlists',
+                            cascade='all, delete')
+
+    def save(self):
+        """Save bucketlist object"""
+        db.session.add(self)
+        db.session.commit
+        
+    def delete(self):
+        """Delete bucketlist object"""
+        db.session.delete(self)
+        db.session.commit
+
+    def __repr__(self):
+            return '<Bucketlist: {}>'.format(self.name)
+
+class Item(db.Model):
+    """Define item properties."""
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'))
+    created_on = db.Column(db.DateTime, default=datetime.now)
+    bucketlist = db.relationship('Bucketlist', backref='items',
+                            cascade='all, delete')
+
+    def save(self):
+        """Save item object."""
+        db.session.add(self)
+        db.session.commit
+
+    def delete(self):
+        """Delete item object """
+        db.session.delete(self)
+        db.session.commit
+
+    def __repr__(self):
+            return '<Item: {}>'.format(self.name)
